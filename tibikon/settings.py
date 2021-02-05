69038396d9f27d904a6dd6a7e904bd85987baec3
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2021-02-05 21:05:09 +0200

    arabic numbers

--- tibikon/settings.py
+++ tibikon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -106,16 +107,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ar-ir'
+LANGUAGE_CODE = 'ar-sy'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Damascus'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- tibikon/views.py
+++ tibikon/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .funcs import Feast as tbk
 import datetime as dt
+from django import template                                                             
+
+register = template.Library()                                                           
+
+@register.filter(name='persianize_digits')                                              
+def persian_int(string):                                                                
+    persianize = dict(zip("0123456789",'۰۱۲۳۴۵۶۷۸۹'))                                   
+    return ''.join(persianize[digit] if digit in persianize else digit for digit in str(string))
 
 def home(request):
 	if request.method == "POST":
